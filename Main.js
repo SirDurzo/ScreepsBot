0635d377ead684a6a88908cc8b99cfd2faedcf40
SirDurzo <flannery.mike@gmail.com>
2015-08-13 11:21:31 -0400

    Update Main.js
    added creep automation logic... currently not working

--- Main.js
+++ Main.js
@@ -3,6 +3,22 @@ var guard = require('guard');
 var builder = require('builder');
 var upgrade = require('upgrade')
 
+/* ---Still trying to get this to work
+var hc = 0;
+var uc = 0;
+var bc = 0;
+
+while ( Games.creeps < 10 ){
+	if (Memory.role.'harvester' < 3)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester[hc++]', { role: 'harvester' } );
+	else if (Memory.role.'upgrade' < 3)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade[uc++]', { role: 'upgrade' } );
+	else if (Memory.role.'builder' < 1)
+		Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder[bc++]', {role: 'builder'} );
+	else if (Memory.role.'harvester' < 5 && Memory.role.'upgrade' == 2 && Memory.role.'builder' == 1)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester[hc++]', { role: 'harvester' } );
+}
+*/
 for(var name in Game.creeps) {
 	var creep = Game.creeps[name];
 
@@ -21,3 +37,6 @@ for(var name in Game.creeps) {
 	    upgrade(creep);
 	}
 }
+
+
+
