d3a77025b92e2bbad693d9ecfd49ade0545076bf
SirDurzo <flannery.mike@gmail.com>
2015-08-13 11:56:29 -0400

    Update Main.js
    added working automation but with bugs

--- Main.js
+++ Main.js
@@ -3,22 +3,24 @@ var guard = require('guard');
 var builder = require('builder');
 var upgrade = require('upgrade')
 
-/* ---Still trying to get this to work
+//automatically spawning - however hc isnt incrementing... maybe try a for loop instead of if/else
 var hc = 0;
 var uc = 0;
 var bc = 0;
-
-while ( Games.creeps < 10 ){
-	if (Memory.role.'harvester' < 3)
-		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester[hc++]', { role: 'harvester' } );
-	else if (Memory.role.'upgrade' < 3)
-		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade[uc++]', { role: 'upgrade' } );
-	else if (Memory.role.'builder' < 1)
-		Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder[bc++]', {role: 'builder'} );
-	else if (Memory.role.'harvester' < 5 && Memory.role.'upgrade' == 2 && Memory.role.'builder' == 1)
-		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester[hc++]', { role: 'harvester' } );
+if(!Game.spawns.Spawn1.spawning) {
+	if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 3)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + [hc++], { role: 'harvester' } );
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length < 3)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade' + [uc++], { role: 'upgrade' } );
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'builder'; }}).length >)
+		Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + [bc++], {role: 'builder'} );
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 5 && 
+	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length == 2 && 
+	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'builder'; }}).length == 1)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + [hc++], { role: 'harvester' } );
 }
-*/
+
+
 for(var name in Game.creeps) {
 	var creep = Game.creeps[name];
 
