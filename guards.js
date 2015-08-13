323914673bc1831ccd979f195360c9def5988f02
SirDurzo <flannery.mike@gmail.com>
2015-08-13 07:58:25 -0400

    Update and rename guards to guards.js
    adjusted method syntax
    
    added js extension

--- guards.js
+++ guards.js
@@ -1,5 +1,5 @@
 module.exports = function (creep) {
-    var targets = creep.room.find(Game.HOSTILE_CREEPS);
+    var targets = creep.room.find(FIND_HOSTILE_CREEPS);
 	if(targets.length) {
 		creep.moveTo(targets[0]);
 		creep.attack(targets[0]);
