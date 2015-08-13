588040c9c31c50774423624d51613481af01c4b1
SirDurzo <flannery.mike@gmail.com>
2015-08-13 07:45:37 -0400

    syntax fix
    for(var name in Game.creeps){
        var creep = Game.creeps[name];
    
    variable cannot start with 'creep'
    
    if(creep.energy < creep.energyCapacity) {
    if(creep.carry.energy < creep.carryCapacity) { <---- correct syntax
    
    var sources = creep.room.find(Game.SOURCES);
    var sources = creep.room.find(FIND_SOURCES); <----correct method
    
    and added 'js' extension

--- harvesters.js
+++ harvesters.js
@@ -1,7 +1,7 @@
 module.exports = function (creep) {
 
-	if(creep.energy < creep.energyCapacity) {
-		var sources = creep.room.find(Game.SOURCES);
+	if(creep.carry.energy < creep.carryCapacity) {
+		var sources = creep.room.find(FIND_SOURCES);
 		creep.moveTo(sources[0]);
 		creep.harvest(sources[0]);
 	}
