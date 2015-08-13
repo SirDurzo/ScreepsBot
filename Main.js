38328769c3a7ec87b743c5ffda6df2c4ef76ed10
SirDurzo <flannery.mike@gmail.com>
2015-08-13 07:47:19 -0400

    Update Main.js
    for(var name in Game.creeps) {
    	var creep = Game.creeps[name];
    
    'creep' cannot be a standalone variable

--- Main.js
+++ Main.js
@@ -2,8 +2,8 @@ var harvester = require('harvesters');
 var guard = require('guards');
 var builder = require('builders');
 
-for(var creepName in Game.creeps) {
-	var creep = Game.creeps[creepName];
+for(var name in Game.creeps) {
+	var creep = Game.creeps[name];
 
 	if(creep.memory.role == 'harvester') {
 		harvester(creep);
