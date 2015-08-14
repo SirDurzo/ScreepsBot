457b8380ff25c01d93acc6624d4c784da196bb8e
SirDurzo <flannery.mike@gmail.com>
2015-08-14 07:28:46 -0400

    Update Main.js
    removed jobs code block and added module

--- Main.js
+++ Main.js
@@ -7,25 +7,7 @@ var upgrade = require('upgrade')
 //Now with working creep automation. needed to initialize Memory.count
 
 if(!Game.spawns.Spawn1.spawning) {
-    
-    Memory.count = (Memory.count || 0) + 1;
-    
-	if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 4)
-		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
-        
-    
-	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length < 4)
-		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade' + Memory.count, { role: 'upgrade' } );
-
-	
-	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 7 && 
-	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length >= 3) 
-	    Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
-
-
-	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'builder'; }}).length < 1)
-		Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + Memory.count, { role: 'builder'} );
-		
+	jobs();
 }
 
 for(var name in Game.creeps) {
