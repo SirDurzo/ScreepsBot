2f2ab079b6072eb53e5c01fd31cee6d38d70151f
SirDurzo <flannery.mike@gmail.com>
2015-08-14 08:02:11 -0400

    Update Main.js
    added new harvester and collector logic

--- Main.js
+++ Main.js
@@ -1,13 +1,13 @@
-
 var harvester = require('harvester');
+var collector = require('collector');
 var guard = require('guard');
 var builder = require('builder');
-var upgrade = require('upgrade')
-
-//Now with working creep automation. needed to initialize Memory.count
+var upgrade = require('upgrade');
+var jobs = require('jobs');
+var sysinitharvester = require('sysinitharvester');
 
 if(!Game.spawns.Spawn1.spawning) {
-	jobs();
+    jobs();
 }
 
 for(var name in Game.creeps) {
@@ -16,17 +16,20 @@ for(var name in Game.creeps) {
 	if(creep.memory.role == 'harvester') {
 		harvester(creep);
 	}
-
 	if(creep.memory.role == 'builder') {
 	    builder(creep);
 	}
-	
 	if(creep.memory.role == 'guard') {
 	    guard(creep);
 	}
 	if(creep.memory.role == 'upgrade') {
 	    upgrade(creep);
 	}
+	if(creep.memory.role == 'collector') {
+	    collector(creep);
+	}
+	if(creep.memory.role == 'sysinitharvester') {
+	    sysinitharvester(creep);
+	}
 }
-
 
