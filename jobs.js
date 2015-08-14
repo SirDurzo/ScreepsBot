945a970aed611d151d97ed7a53f19bd9e1b8371d
SirDurzo <flannery.mike@gmail.com>
2015-08-14 08:03:55 -0400

    Update jobs.js
    added new collector role, and updated logic to incorporate a wipe of the system or a fresh start.

--- jobs.js
+++ jobs.js
@@ -1,18 +1,25 @@
 module.exports = function (creep) {
 
 Memory.count = (Memory.count || 0) + 1;
-    
-	if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 7)
+
+	if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'sysinitharvester'; }}).length < 3 &&
+	    Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length >= 3)  
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'sysinitharvester' } );
+
+	if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 3)
 		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
+		
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'collector'; }}).length < 3)
+		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'collector' } );	
         
     
-	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length < 5)
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length < 3)
 		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade' + Memory.count, { role: 'upgrade' } );
 
-	
-	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length < 10 && 
-	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length >= 5) 
-	    Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
+	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'harvester'; }}).length >= 3 && 
+	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'upgrade'; }}).length >= 5 &&
+	         Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'collector'; }}).length < 6)
+	    Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'collector' } );
 
 
 	else if (Game.spawns.Spawn1.room.find(FIND_MY_CREEPS, { filter: function(creep){ return creep.memory.role == 'builder'; }}).length < 1)
