4ecf216ddc5406227dc4726a16aa3c1028dcb2bf
SirDurzo <flannery.mike@gmail.com>
2015-11-02 20:16:09 -0500

    Update collector.js

--- collector.js
+++ collector.js
@@ -3,11 +3,18 @@ module.exports = function (creep) {
 	var energy = creep.room.find(FIND_DROPPED_ENERGY);
 
 	var extensions = creep.room.find(FIND_MY_STRUCTURES, {
-		filter: { structureType: STRUCTURE_EXTENSION }
-	});
+		filter: { structureType: STRUCTURE_EXTENSION } });
+		
+	var roomStorage = creep.room.find(FIND_MY_STRUCTURES, {
+		filter: { structureType: STRUCTURE_STORAGE } });
 	
 	// collect
     if (creep.carry.energy === 0 && energy.length) {
+        if(energy.length > 1){
+            energy.sort(function (a, b) {
+                return b.energy - a.energy
+            });
+        }
         creep.moveTo(energy[0]);
 		creep.pickup(energy[0]);
 		return;
@@ -31,6 +38,17 @@ module.exports = function (creep) {
 		if (extension.energy < extension.energyCapacity) {
 			creep.moveTo(extension);
 			creep.transferEnergy(extension);
+			return;
+		}
+	}
+	
+		// feed storage
+	for (var index in roomStorage) {
+		var myStorage = roomStorage[index];
+
+		if (myStorage.energy < myStorage.energyCapacity) {
+			creep.moveTo(myStorage);
+			creep.transferEnergy(myStorage);
 			return;
 		}
 	}
