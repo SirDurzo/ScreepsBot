8f4e56286ceb554b385ec125a36094fd14071a9c
SirDurzo <flannery.mike@gmail.com>
2015-11-02 22:58:14 -0500

    Update builder.js

--- builder.js
+++ builder.js
@@ -1,10 +1,10 @@
 module.exports = function(creep) {
     
     if(creep.carry.energy === 0) {
-    	   	var roomStorage = creep.room.find(FIND_MY_STRUCTURES, {
-			filter: { structureType: STRUCTURE_STORAGE } });
+    	   //	var roomStorage = creep.room.find(FIND_MY_STRUCTURES, {
+		   //   filter: { structureType: STRUCTURE_STORAGE } });
 			
-			creep.moveTo(roomStorage);
+			creep.moveTo(creep.room.storage);
 			creep.room.storage.transferEnergy(creep);
 		}
 		else {
