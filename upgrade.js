5b5b16034083a3d90f92b587997c8bff87bb65a9
SirDurzo <flannery.mike@gmail.com>
2015-11-02 20:58:50 -0500

    Update upgrade.js

--- upgrade.js
+++ upgrade.js
@@ -1,11 +1,16 @@
 module.exports = function(creep) {
     
     if(creep.carry.energy === 0) {
-			creep.moveTo(Game.spawns.Spawn1);
-			Game.spawns.Spawn1.transferEnergy(creep);
+    	    		
+    	   	var roomStorage = creep.room.find(FIND_MY_STRUCTURES, {
+			filter: { structureType: STRUCTURE_STORAGE } });
+			
+			creep.moveTo(roomStorage);
+			creep.room.storage.transferEnergy(creep);
 		}
 		else {
 			var mycontroller = creep.room.find(FIND_MY_STRUCTURES);
+			
 			if(mycontroller.length) {
 				creep.moveTo(creep.room.controller);
 				creep.upgradeController(creep.room.controller);
