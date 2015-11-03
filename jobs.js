f0f162841c817c47a54602b429c8ab263c4865b5
SirDurzo <flannery.mike@gmail.com>
2015-11-02 23:00:25 -0500

    Update jobs.js

--- jobs.js
+++ jobs.js
@@ -1,63 +1,61 @@
-module.exports = function (creep) {
-
-//Define roles
-/*
-var roleInitHarvester = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'InitHarvester' + Memory.count, { role: 'initharvester' } );
-var roleHarvester = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
-var roleCollector = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Collector' + Memory.count, { role: 'collector' } );
-var roleUpgrade = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade' + Memory.count, { role: 'upgrade' } );
-var roleBuilder = Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder' + Memory.count, { role: 'builder'} );
-*/
-
-//Create creep variable to determine number of creeps in the room
-var creeps = Game.spawns.Spawn1.room.find(FIND_MY_CREEPS);
-
-//Filter and determine number of each role for all the active creeps in the room
-var countHarvester = _.filter(creeps, function(creep){ return creep.memory.role === 'harvester'}).length
-var countInitharvester = _.filter(creeps, function(creep){ return creep.memory.role === 'initharvester'}).length
-var countCollector = _.filter(creeps, function(creep){ return creep.memory.role === 'collector'}).length
-var countUpgrade = _.filter(creeps, function(creep){ return creep.memory.role === 'upgrade'}).length
-var countBuilder = _.filter(creeps, function(creep){ return creep.memory.role === 'builder'}).length
 
 
-Memory.count = (Memory.count || 0) + 1;
+		var spawn = Game.spawns[index];module.exports = function (creep) {
+	var energy = creep.room.find(FIND_DROPPED_ENERGY);
 
-	 if (countInitharvester < 2 && countHarvester <= 1)  {
-		var result = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'InitHarvester' + Memory.count, { role: 'initharvester' } ) ;
-		//console.log(" Spawning Initial Harvester - " + (countInitharvester + 1) + " of 3 result: " + result);
+	var extensions = creep.room.find(FIND_MY_STRUCTURES, {
+		filter: { structureType: STRUCTURE_EXTENSION } });
+		
+	var roomStorage = creep.room.find(FIND_MY_STRUCTURES, {
+		filter: { structureType: STRUCTURE_STORAGE } });
+	
+	// collect
+    if (creep.carry.energy === 0 && energy.length) {
+        if(energy.length > 1){
+            energy.sort(function (a, b) {
+                return b.energy - a.energy
+            });
+        }
+        creep.moveTo(energy[0]);
+		creep.pickup(energy[0]);
 		return;
+    }
+	
+	// feed spawns
+	for (var index in Game.spawns) {
+
+		if (spawn.energy < spawn.energyCapacity) {
+			creep.moveTo(spawn);
+			creep.transferEnergy(spawn);
+			return;
 		}
+	}
+	
+// feed extensions
+	for (var index in extensions) {
+		var extension = extensions[index];
 
-	else if (countHarvester < 2) {
-		var result = Game.spawns.Spawn1.createCreep( [WORK ,WORK, MOVE], 'Harvester' + Memory.count, { role: 'harvester' } );
-		//console.log(" Spawning Harvester - " + (countHarvester +1) + " of 3 result: " + result);
-		return;
+		if (extension.energy < extension.energyCapacity) {
+			creep.moveTo(extension);
+			creep.transferEnergy(extension);
+			return;
 		}
-			
-	else if (countCollector < 3) {
-		var result = Game.spawns.Spawn1.createCreep( [CARRY, MOVE, CARRY, MOVE], 'Collector' + Memory.count, { role: 'collector' } );
-        //console.log(" Spawning Collector - " + (countCollector +1) + " of 3 result: " + result);
-		return;
 	}
-    
-	else if (countUpgrade < 2) {
-		var result = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Upgrade' + Memory.count, { role: 'upgrade' } );
-		//console.log(" Spawning Upgrader - " + (countUpgrade +1) + " of 3 result: " + result) + ")";
-		return;
-	}
+	
+// feed storage
 
-	/*
-	else if (countHarvester >= 3 && countUpgrade >= 5 && countCollector < 6) {
-	    var result = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Collector' + Memory.count, { role: 'collector' } );
-		console.log(" Spawning Collector - " + (countCollector +1) + " of 6 result: " + result);
-		return;
-	}
-    */
-
-
-	else if (countBuilder < 3) {
-		var result = Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Builder' + Memory.count, { role: 'builder'} );
-		//console.log(" Spawning Builder - " + (countBuilder +1) + " of 3 result: " + result);
-		return;
-	}
+	//for (var index in roomStorage) {
+	   //var myStorage = roomStorage[index];
+       // console.log("before storage if")
+       
+		if (creep.room.storage.store.energy < creep.room.storage.storeCapacity) {
+		    
+		    //var storageLog = creep.room.storage;
+		    //console.log("Storage=" + storageLog);
+		    
+			creep.moveTo(creep.room.storage);
+			creep.transferEnergy(creep.room.storage);
+			return;
+		}
+	//}
 }
